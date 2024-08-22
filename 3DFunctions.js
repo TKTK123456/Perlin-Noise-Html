beedc946a8c42f52076379d8044097056bb3a2a8
TKTK123456 <103334266+TKTK123456@users.noreply.github.com>
2024-08-22 20:02:17 +0000

    updating...

--- 3DFunctions.js
+++ 3DFunctions.js
@@ -1,7 +1,7 @@
 'use strict';
 const engine3D = {
-  translate: (x, y, z) => (v => ({x: v.x + x, y: v.y + y, z: v.z + z})),
-  distance: (c1, c2) => ((c2.x - c1.x)**2 + (c2.y - c1.y)**2 + (c2.z - c1.z)**2) ** 0.5,
+  translate: (x, y, z) => (v => ({ x: v.x + x, y: v.y + y, z: v.z + z })),
+  distance: (c1, c2) => ((c2.x - c1.x) ** 2 + (c2.y - c1.y) ** 2 + (c2.z - c1.z) ** 2) ** 0.5,
   dot_prod: (v1, v2) => v1.x * v2.x + v1.y * v2.y + v1.z * v2.z,
   x_axis_rotate: (r) => (v => ({
     x: v.x,
@@ -25,24 +25,31 @@ const engine3D = {
   }),
   drawVert: function(vert, canvas, color, width, height, cW, cH) {
     let context = canvas.getContext('2d');
-      let v = this.point(vert, cW/2, cH/2);
-      context.fillStyle = color;
-      context.fillRect(v.x, v.y, width, height);
+    let v = this.point(vert, cW / 2, cH / 2);
+    context.fillStyle = color;
+    context.fillRect(v.x, v.y, width, height);
   },
   render: function(canvas, world, cW, cH, cam) {
     let context = canvas.getContext('2d');
     context.clearRect(0, 0, cW, cH);
     for (let i = 0; i < world.length; i++) {
       world[i].dist = this.distance(cam, world[i].vert);
-      world[i].c_vert = {x: (world[i].vert.x - cam.x) / world[i].dist,
-           y: (world[i].vert.y - cam.y) / world[i].dist,
-           z: (world[i].vert.z - cam.z) / world[i].dist};
+      world[i].c_vert = {
+        x: (world[i].vert.x - cam.x) / world[i].dist,
+        y: (world[i].vert.y - cam.y) / world[i].dist,
+        z: (world[i].vert.z - cam.z) / world[i].dist
+      };
     }
-    world = world.filter(f => 
+    world = world.filter(f =>
       (wireframe || !has_vects || this.dot_prod(f.c_vect, f.vect) < 0) &&
-        f.verts.some(c => 
-          this.dot_prod({x: c.x-cam.x,
-                         y: c.y-cam.y,
-                         z: c.z-cam.z}, cam_vect) > 0));
+      f.verts.some(c =>
+        this.dot_prod({
+          x: c.x - cam.x,
+          y: c.y - cam.y,
+          z: c.z - cam.z
+        }, cam_vect) > 0));
+    for (let i = 0; i < world.length; i++) {
+      this.drawVert(world[i].c_vert, canvas, world[i].color, world[i].width, world[i].height, cW, cH)
+    }
   }
 };
